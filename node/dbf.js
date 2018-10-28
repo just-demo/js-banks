7d26fb67a3ba0a8ceee83fccab3ac4909671d60f
Anatolii <anatolii.kryvda@gmail.com>
2018-10-28 18:46:04 +0200

    dbf reader

--- node/dbf.js
+++ node/dbf.js
@@ -1,32 +1,29 @@
-// let Parser = require('node-dbf');
-let Parser = require('dbf-parser');
-// let Parser = require('node-dbf');
+let Parser = require('node-dbf').default;
+let iconv = require('iconv-lite');
+// import Parser from 'node-dbf'
+// let Parser = require('dbf-parser');
 
-['ascii', 'utf8', 'utf16le', 'ucs2', 'base64', 'binary', 'hex'].forEach(encoding => {
-console.log('========================', encoding, '===============================');
+let parser = new Parser('/home/pc/Downloads/RCUKRU.DBF', {encoding: 'binary'});
 
-let parser = new Parser('RCUKRU.DBF', {encoding: encoding});
-
-parser.on('start', (p) => {
-    console.log('dBase file parsing has started');
-});
-
-parser.on('header', (h) => {
-    console.log('dBase file header has been parsed');
-});
-
-let first = true;
+const banks = [];
 parser.on('record', (record) => {
-    if (first) {
-        console.log('Record: ', record.SHORTNAME);
-        first = false;
-    }
+    banks.push({
+        shortName: decode(record.SHORTNAME),
+        fullName: decode(record.FULLNAME)
+    });
 });
 
-parser.on('end', (p) => {
-    console.log('Finished parsing the dBase file');
+parser.on('end', () => {
+    console.log(JSON.stringify(banks));
 });
 
 parser.parse();
 
-});
+function decode(value) {
+    return iconv.decode(iconv.encode(value, 'binary'), 'cp866')
+        .replace(/Ї/g, 'Є')
+        .replace(/°/g, 'Ї')
+        .replace(/∙/g, 'ї')
+        .replace(/Ў/g, 'І')
+        .replace(/ў/g, 'і');
+}
