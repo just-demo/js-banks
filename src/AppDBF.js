e343080a20798c3078000eb980a78b574e3a5eaa
Anatolii <anatolii.kryvda@gmail.com>
2018-11-21 00:22:23 +0200

    reuse third party react-table

--- src/AppDBF.js
+++ src/AppDBF.js
@@ -1,33 +1,56 @@
 import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
+import ReactTable from 'react-table';
+import 'react-table/react-table.css'
 
 class AppDBF extends Component {
     constructor(props) {
         super(props);
         this.state = {banks: []};
+    }
+
+    componentDidMount() {
         fetch('/dbf.json')
             .then(banks => banks.json())
             .then(banks => this.setState({banks: banks}));
     }
 
     render() {
+        const columns = (this.state.banks[0] || []).map((value, index) => {
+            return {
+                id: value,
+                Header: value,
+                accessor: values => values[index]
+            };
+        });
+        const data = this.state.banks.slice(1);
+        console.log(data.length);
+
         return (
-            <div>
-                <table className="banks">
-                    <tbody>
-                    {this.filter(this.state.banks).map((bank, index) => (
-                        <tr>
-                            <td>{index}</td>
-                            {bank.map(field => (
-                                <td style={{whiteSpace: 'nowrap'}}>{field}</td>
-                            ))}
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            </div>
+            <ReactTable
+                data={data}
+                columns={columns}
+                pageSizeOptions={[10, 100]}
+                defaultPageSize={100}
+            />
         );
+        // return (
+        //     <div>
+        //         <table className="banks">
+        //             <tbody>
+        //             {this.filter(this.state.banks).map((bank, index) => (
+        //                 <tr>
+        //                     <td>{index}</td>
+        //                     {bank.map(field => (
+        //                         <td style={{whiteSpace: 'nowrap'}}>{field}</td>
+        //                     ))}
+        //                 </tr>
+        //             ))}
+        //             </tbody>
+        //         </table>
+        //     </div>
+        // );
     }
 
     filter(data) {
