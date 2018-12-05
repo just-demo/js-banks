e88515e230a353e684ac6e13f42a445d3fa3ea9f
Anatolii Kryvda <anatolii.kryvda@automationanywhere.com>
2018-12-05 15:15:52 +0200

    Ratings page - cleanup and refactoring

--- src/AppRatings.js
+++ src/AppRatings.js
@@ -11,28 +11,15 @@ class AppRatings extends Component {
         super(props);
         this.state = {
             scale: 1,
-            banks: {},
+            banks: [],
             ratings: {}
         };
-    }
-
-    convertBanks(banks) {
-        return _.keyBy(banks.map(bank => {
-            return {
-                id: bank.internal.id.minfin,
-                name: bank.name.minfin,
-                site: (bank.site.minfin || [])[0],
-                link: bank.internal.link.minfin,
-                dateOpen: bank.dateOpen,
-                dateIssue: bank.dateIssue
-            };
-        }).filter(bank => bank.name), 'id');
     }
 
     componentDidMount() {
         fetch('/banks.json')
             .then(banks => banks.json())
-            .then(banks => this.setState({banks: this.convertBanks(banks)}));
+            .then(banks => this.setState({banks: banks}));
 
 
         fetch('/minfin-ratings.json')
@@ -42,31 +29,25 @@ class AppRatings extends Component {
         console.log(t.hello());
     }
 
-    compare(a, b) {
-        return _.isUndefined(a) ?
-            _.isUndefined(b) ? 0 : -1 :
-            _.isUndefined(b) ? 1 :
-                a > b ? 1 : a < b ? -1 : 0;
-    }
-
     render() {
         const start = new Date();
         this.dates = Object.keys(this.state.ratings).sort().reverse();
-
-        const latestRating = {};
-        _.forOwn(this.state.ratings, (dateRating, date) => {
-            _.forOwn(dateRating, (bankRating, bankId) => {
-                if (!latestRating[bankId] || latestRating[bankId].date < date) {
-                    latestRating[bankId] = {
-                        date: date,
-                        rating: bankRating
-                    };
-                }
-            });
-        });
+        this.banks = _.keyBy(this.state.banks.map(bank => {
+            const datesIssue = Object.values(bank.dateIssue);
+            return {
+                id: bank.internal.id.minfin,
+                name: bank.name.minfin,
+                site: (bank.site.minfin || [])[0],
+                link: bank.internal.link.minfin,
+                dateOpen: this.projectDate(bank.dateOpen.dbf),
+                dateClosed: this.projectDate(bank.dateIssue.pdf),
+                dateIssueMin: this.projectDate(_.min(datesIssue)),
+                dateIssueMax: this.projectDate(_.max(datesIssue))
+            };
+        }).filter(bank => bank.name), 'id');
 
         // Sort by latest rating in reverse order
-        const bankIds = Object.keys(this.state.banks).sort((bankId1, bankId2) => {
+        const bankIds = Object.keys(this.banks).sort((bankId1, bankId2) => {
             for (const date of this.dates) {
                 const dateRating = this.state.ratings[date];
                 const diff = this.compare(dateRating[bankId1], dateRating[bankId2]);
@@ -74,6 +55,13 @@ class AppRatings extends Component {
                     return diff;
                 }
             }
+
+            return this.compareBy(this.banks[bankId1], this.banks[bankId2], {
+                dateIssueMin: true,
+                dateIssueMax: true,
+                dateOpen: false,
+                dateClosed: true
+            });
         }).reverse();
 
         const r = (
@@ -90,10 +78,10 @@ class AppRatings extends Component {
                     </tr>
                     {bankIds.map(bankId => (
                         <tr key={bankId}>
-                            <td><a href={this.state.banks[bankId].link}>{this.state.banks[bankId].name}</a></td>
-                            <td><a href={this.state.banks[bankId].site}>{((this.state.banks[bankId].site || '').match(/\/\/([^/]+)/) || [])[1]}</a></td>
+                            <td><a href={this.banks[bankId].link}>{this.banks[bankId].name}</a></td>
+                            <td><a href={this.banks[bankId].site}>{((this.banks[bankId].site || '').match(/\/\/([^/]+)/) || [])[1]}</a></td>
                             {this.dates.map(date => (
-                                <td key={date} className={this.classForCell(bankId, date)} style={this.styleForCell(bankId, date)}>
+                                <td key={date} className={this.classForCell(this.banks[bankId], date)} style={this.styleForCell(this.state.ratings[date][bankId])}>
                                     <div>
                                         {this.state.ratings[date][bankId] || '-'}
                                     </div>
@@ -111,6 +99,23 @@ class AppRatings extends Component {
         return r;
     }
 
+    compare(a, b) {
+        return _.isUndefined(a) ?
+            _.isUndefined(b) ? 0 : -1 :
+            _.isUndefined(b) ? 1 :
+                a > b ? 1 : a < b ? -1 : 0;
+    }
+
+    compareBy(obj1, obj2, fields) {
+        for (const field of  Object.keys(fields)) {
+            const diff = this.compare(obj1[field], obj2[field]);
+            if (diff) {
+                return fields[field] ? diff : -diff;
+            }
+        }
+        return 0;
+    }
+
     projectDate(date) {
         if (!date) {
             return date;
@@ -125,34 +130,26 @@ class AppRatings extends Component {
         return projected;
     }
 
-    classForCell(bankId, date) {
-        // TODO: init or cache projectDate
-        const dateOpen = this.projectDate(this.state.banks[bankId].dateOpen.dbf);
-        const dateClosed = this.projectDate(this.state.banks[bankId].dateIssue.pdf);
-        const datesIssue = Object.values(this.state.banks[bankId].dateIssue);
-        const dateIssueMin = this.projectDate(_.min(datesIssue));
-        const dateIssueMax = this.projectDate(_.max(datesIssue));
-
+    classForCell(bank, date) {
         return classNames({
             'rating': true,
-            'issue': dateIssueMax >= date && date >= dateIssueMin,
-            'issue-max': dateIssueMax === date,
-            'issue-min': dateIssueMin === date,
-            'closed': (dateClosed && date > dateIssueMax) || (dateOpen && date < dateOpen),
-            'open': dateOpen && dateOpen === date
+            'issue': bank.dateIssueMax >= date && date >= bank.dateIssueMin,
+            'issue-max': bank.dateIssueMax === date,
+            'issue-min': bank.dateIssueMin === date,
+            'closed': (bank.dateClosed && date > bank.dateIssueMax) || (bank.dateOpen && date < bank.dateOpen),
+            'open': bank.dateOpen && bank.dateOpen === date
         });
     }
 
-    styleForCell(bankId, date) {
-        let rating = this.state.ratings[date][bankId];
+    styleForCell(rating) {
         if (!rating) {
             return {};
         }
 
         // TODO: show range per color matrix with color=from-to
-        const max = 5; // green - rgb(0, 128, 0)
+        const max = 5;    // green  - rgb(  0, 128, 0)
         const middle = 3; // yellow - rgb(255, 255, 0)
-        const min = 1; // red - rgb(255, 0, 0)
+        const min = 1;    // red    - rgb(255,   0, 0)
         const scale = this.state.scale;
         rating = Math.max(min, Math.min(max, rating)); // truncate
         rating = Math.floor(rating * scale) / scale; // resolution
@@ -160,7 +157,6 @@ class AppRatings extends Component {
         const green = rating >= middle ? this.scale(rating, middle, 255, max, 128) : this.scale(rating, min, 0, middle, 255);
         const blue = 0;
         return {backgroundColor: `rgb(${red}, ${green}, ${blue})`};
-        // return {backgroundColor: `yellow`};
     }
 
     scale(key, minKey, minValue, maxKey, maxValue) {
